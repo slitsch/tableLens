4e9fdc4aafe0fe6006dac1fc3ef1eb226862f1d5
Sam <knitz4sanity@gmail.com>
2014-09-10 16:23:07 -0600

    Start of tableLens prj

---
+++ js/tableLens.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Valued Customer on 9/9/2014.
+ */
+
+//var numRecords = 0
+//
+////read the data file - hard coded for now
+//$.getJSON("data/white_peg31.json", function(data) {
+//    console.log(data);
+//    // data is a JavaScript object now. Handle it as such
+//    numRecords = data[0].Average;
+//    console.log(numRecords);
+//});
